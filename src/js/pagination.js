b45482730421a95fd96c3c0125509743e5b87c2c
Oleksii <oleksii.ivanichok@gmail.com>
2023-10-14 18:09:17 +0300

    3 task working on

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,6 @@
-export function paginationStatistic(tableUsers){
+import {renderStatistics} from "./domOperation";
+
+export function paginationStatistic(tableUsers) {
   let state = {
     'querySet': tableUsers,
     'page': 1,
@@ -6,9 +8,11 @@ export function paginationStatistic(tableUsers){
   }
   let data = pagination(state.querySet, state.page, state.rows)
   console.log('data:', data);
+  pageButtons();
 
-  let myList = data.querySet
-  function pagination(querySet, page, rows){
+  let elementsToRender = data.querySet;
+
+  function pagination(querySet, page, rows) {
     let trimStart = (page - 1) * rows;
     let trimEnd = trimStart + rows;
 
@@ -20,10 +24,36 @@ export function paginationStatistic(tableUsers){
       'querySet': trimmedData,
       'pages': pages
     }
-    function pageButtons(){
-      let tableContent = document.getElementById()
+  }
+
+  function pageButtons(pages) {
+
+    // let statisticContainer = document.getElementById('statisticContainer');
+    let paginationContainer = document.getElementById('paginationContainer');
+    paginationContainer.innerHTML = '';
+
+    for (let page = 1; page <= pages; page++) {
+      const paginationElement = document.createElement("li");
+      paginationElement.innerText = `${page}`;
+      console.log(state.page);
+      console.log(page);
+
+      if (page === state.page) {
+        paginationElement.classList.add("current__page");
+      }
+      if (page === pages) {
+        paginationElement.innerText = `Last`;
+      }
+      paginationElement.addEventListener('click', () => {
+        state.page = page;
+        renderStatistics(elementsToRender);
+        console.log(state.page);
+      })
+
+      paginationContainer.appendChild(paginationElement)
     }
 
   }
+
 }
 
